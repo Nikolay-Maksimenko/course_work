bd1ff3206d778e0b0ac17509e59b3a9083fc7917
Nikolay-Maksimenko <horrendos@mail.ru>
2021-10-06 08:17:00 +0300

    adjustment of work

--- Сourse_work.py
+++ Сourse_work.py
@@ -1,91 +1,96 @@
 import requests
 import json
 
-class Vk_user:
-    def __init__(self, vk_token, vk_id):
-        self.vk_token = vk_token
-        self.vk_id = vk_id
+class VKUser:
+    VK_API_BASE_URL = 'https://api.vk.com/method/'
+
+    def __init__(self, token, user_id, api_version):
+        self.token = token
+        self.user_id = user_id
+        self.api_version = api_version
+        self.user_id = self.get_user_id()
 
     def get_user_id(self):
-        url = 'https://api.vk.com/method/users.get'
-        params = {'user_ids': self.vk_id, 'access_token': self.vk_token, 'v': '5.131'}
+        url = self.VK_API_BASE_URL + 'users.get'
+        params = {'user_ids': self.user_id, 'access_token': self.token, 'v': self.api_version}
         result = requests.get(url, params=params)
         return result.json()['response'][0]['id']
 
-    def get_albums(self, vk_id):
-        url = 'https://api.vk.com/method/photos.getAlbums'
-        params = {'owner_id': vk_id, 'need_system': 1, 'access_token': self.vk_token, 'v': '5.131'}
+    def get_albums(self):
+        url = self.VK_API_BASE_URL + 'photos.getAlbums'
+        params = {'owner_id': self.user_id, 'need_system': 1, 'access_token': self.token, 'v': self.api_version}
         result = requests.get(url, params=params)
-        list_of_albums = result.json()['response']['items']
-        id_albums = [id['id'] for id in list_of_albums]
-        # print(id_albums)
-        return id_albums
+        albums_list = result.json()['response']['items']
+        return [album['id'] for album in albums_list]
 
-    def get_list_photos(self, number_of_photos, album_id='profile'):
-        vk_id = self.get_user_id()
-        list_of_albums = self.get_albums(vk_id)
-        if album_id != 'profile' and album_id not in list_of_albums:
+    def get_photos(self, count, album_id='profile'):
+        albums = self.get_albums()
+        if album_id != 'profile' and album_id not in albums:
             raise Exception('неверный id альбома')
-        url = 'https://api.vk.com/method/photos.get'
-        params = {'owner_id' : vk_id, 'album_id': album_id, 'extended' : 1, 'count': number_of_photos, 'access_token' : self.vk_token, 'v' : '5.131'}
+        url = self.VK_API_BASE_URL + 'photos.get'
+        params = {'owner_id': self.user_id, 'album_id': album_id, 'extended': 1, 'count': count, 'access_token': self.token, 'v': self.api_version}
         result = requests.get(url, params=params)
-        list_of_photos = result.json()['response']['items']
-        return list_of_photos
+        return result.json()['response']['items']
 
-    def get_max_size_photos(self, list_of_photos):
+    def get_max_size_photos(self, photos):
         max_size_photos = []
-        for photo in list_of_photos:
+        for photo in photos:
             max_size_photos.append({'url': photo['sizes'][-1]['url'], 'likes_count': photo['likes']['count'], 'date': photo['date'], 'size_type': photo['sizes'][-1]['type']})
         return max_size_photos
 
-    def get_photos_names(self, list_of_photos):
+    def get_photos_names(self, photos):
         photo_names = []
-        for photo in list_of_photos:
-            name = str(photo['likes_count']) + '.jpg'
+        for photo in photos:
+            name = str(photo['likes']['count']) + '.jpg'
             if name in photo_names:
                 name = str(photo['date']) + name
             photo_names.append(name)
         return photo_names
 
-class Yandex_disk:
-    def __init__(self, yd_token):
-        self.yd_token = yd_token
-        self.yd_headers = {'Authorization': 'OAuth ' + self.yd_token}
+    def get_photos_for_upload(self, count = 5):
+        number = 0
+        list_photos = []
+        photos = self.get_photos(count)
+        names = self.get_photos_names(photos)
+        max_size_photos = self.get_max_size_photos(photos)
+        for photo in max_size_photos:
+            list_photos.append({'url': photo['url'], 'name': names[number], 'size_type': photo['size_type']})
+            number+=1
+        return list_photos
+
+class YandexDiscClient:
+    YANDEX_DISC_BASE_URL = 'https://cloud-api.yandex.net:443/v1/disk/resources/'
+
+    def __init__(self, token):
+        self.yd_token = token
+        self.headers = {'Authorization': 'OAuth ' + self.yd_token}
 
     def create_folder(self, folder_name='VK_photos'):
         url = "https://cloud-api.yandex.net:443/v1/disk/resources/"
         params = {'path': folder_name}
-        result = requests.put(url, headers=self.yd_headers, params=params)
+        requests.put(self.YANDEX_DISC_BASE_URL, headers=self.headers, params=params)
         return folder_name
 
     def write_log(self, log):
         with open('log.json', 'a') as file:
             json.dump(log, file, indent=2)
 
-    def upload(self):
+    def upload(self, photos):
         log = []
-        with open('VK_token.txt') as file_object:
-            vk_token = file_object.read().strip()
-        user_id = input("Введите id или username пользователя: ")
-        list_photo = Vk_user(vk_token, user_id)
-        number_of_photos = input("Введите количество загружаемых фотографий: ")
-        all_photos = list_photo.get_list_photos(int(number_of_photos))
-        folder_name = self.create_folder()
-        max_size_photos = list_photo.get_max_size_photos(all_photos)
-        photo_names = list_photo.get_photos_names(max_size_photos)
-        photos_url = [photo['url'] for photo in max_size_photos]
-        photos_size = [photo['size_type'] for photo in max_size_photos]
-        number = 0
-        for photo in photos_url:
-            url = "https://cloud-api.yandex.net:443/v1/disk/resources/upload"
-            params = {'path': folder_name + '/' + photo_names[number], 'url': photos_url[number]}
-            r = requests.post(url, headers=self.yd_headers, params=params)
-            log.append({"file_name": photo_names[number], "size": photos_size[number]})
-            number += 1
-        result = self.write_log(log)
+        url = self.YANDEX_DISC_BASE_URL + "upload"
+        folder = self.create_folder()
+        for photo in photos:
+            params = {'path': folder + '/' + photo['name'], 'url': photo['url']}
+            requests.post(url, headers=self.headers, params=params)
+            log.append({"file_name": photo['name'], "size": photo['size_type']})
+        self.write_log(log)
 
-
-
-upload_photos = Yandex_disk('token')
-result = upload_photos.upload()
-
+if __name__ == '__main__':
+    user_id = input("Введите id или username пользователя: ")
+    count = int(input("Введите количество загружаемых фотографий: "))
+    with open('VK_token.txt') as file_object:
+        vk_token = file_object.read().strip()
+    vk = VKUser(vk_token, user_id, '5.131')
+    photos = vk.get_photos_for_upload(count)
+    yandex = YandexDiscClient('AQAAAAASWvBAAADLWx_UdUb3f0_2iOpcMWQ6JW4')
+    yandex.upload(photos)
