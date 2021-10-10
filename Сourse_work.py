057be15f910d4a94b4dd2c5ea61b79ff5dac9995
Nikolay-Maksimenko <horrendos@mail.ru>
2021-10-10 18:38:04 +0300

    add annotations

--- Сourse_work.py
+++ Сourse_work.py
@@ -1,29 +1,30 @@
 import requests
 import json
+import typing
 
 class VKUser:
     VK_API_BASE_URL = 'https://api.vk.com/method/'
 
-    def __init__(self, token, user_id, api_version):
+    def __init__(self, token: str, user_id: str, api_version: str):
         self.token = token
         self.user_id = user_id
         self.api_version = api_version
         self.user_id = self.get_user_id()
 
-    def get_user_id(self):
+    def get_user_id(self) -> int:
         url = self.VK_API_BASE_URL + 'users.get'
         params = {'user_ids': self.user_id, 'access_token': self.token, 'v': self.api_version}
         result = requests.get(url, params=params)
         return result.json()['response'][0]['id']
 
-    def get_albums(self):
+    def get_albums(self) -> typing.List[str]:
         url = self.VK_API_BASE_URL + 'photos.getAlbums'
         params = {'owner_id': self.user_id, 'need_system': 1, 'access_token': self.token, 'v': self.api_version}
         result = requests.get(url, params=params)
         albums_list = result.json()['response']['items']
         return [album['id'] for album in albums_list]
 
-    def get_photos(self, count, album_id='profile'):
+    def get_photos(self, count: int, album_id: str ='profile') -> typing.List[typing.Dict]:
         albums = self.get_albums()
         if album_id != 'profile' and album_id not in albums:
             raise Exception('неверный id альбома')
@@ -32,13 +33,13 @@ class VKUser:
         result = requests.get(url, params=params)
         return result.json()['response']['items']
 
-    def get_max_size_photos(self, photos):
+    def get_max_size_photos(self, photos: typing.List[typing.Dict]) -> typing.List[typing.Dict]:
         max_size_photos = []
         for photo in photos:
             max_size_photos.append({'url': photo['sizes'][-1]['url'], 'likes_count': photo['likes']['count'], 'date': photo['date'], 'size_type': photo['sizes'][-1]['type']})
         return max_size_photos
 
-    def get_photos_names(self, photos):
+    def get_photos_names(self, photos: typing.List[typing.Dict]) -> typing.List[str]:
         photo_names = []
         for photo in photos:
             name = str(photo['likes']['count']) + '.jpg'
@@ -47,7 +48,7 @@ class VKUser:
             photo_names.append(name)
         return photo_names
 
-    def get_photos_for_upload(self, count = 5):
+    def get_photos_for_upload(self, count: int = 5) -> typing.Dict:
         number = 0
         list_photos = []
         photos = self.get_photos(count)
@@ -61,21 +62,21 @@ class VKUser:
 class YandexDiscClient:
     YANDEX_DISC_BASE_URL = 'https://cloud-api.yandex.net:443/v1/disk/resources/'
 
-    def __init__(self, token):
+    def __init__(self, token: str):
         self.yd_token = token
         self.headers = {'Authorization': 'OAuth ' + self.yd_token}
 
-    def create_folder(self, folder_name='VK_photos'):
+    def create_folder(self, folder_name: str ='VK_photos'):
         url = "https://cloud-api.yandex.net:443/v1/disk/resources/"
         params = {'path': folder_name}
         requests.put(self.YANDEX_DISC_BASE_URL, headers=self.headers, params=params)
         return folder_name
 
-    def write_log(self, log):
+    def write_log(self, log: typing.List[typing.Dict]):
         with open('log.json', 'a') as file:
             json.dump(log, file, indent=2)
 
-    def upload(self, photos):
+    def upload(self, photos: typing.Dict):
         log = []
         url = self.YANDEX_DISC_BASE_URL + "upload"
         folder = self.create_folder()
@@ -92,5 +93,5 @@ if __name__ == '__main__':
         vk_token = file_object.read().strip()
     vk = VKUser(vk_token, user_id, '5.131')
     photos = vk.get_photos_for_upload(count)
-    yandex = YandexDiscClient('AQAAAAASWvBAAADLWx_UdUb3f0_2iOpcMWQ6JW4')
+    yandex = YandexDiscClient('Token')
     yandex.upload(photos)
